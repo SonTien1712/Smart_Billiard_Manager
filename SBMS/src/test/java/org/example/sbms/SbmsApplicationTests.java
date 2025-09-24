29683155f70f06a9754832946cefd267dd8a18ae
Nguyen Tien Son <ntson171201@gmail.com>
2025-09-24 12:43:00 +0700

    Smart Billiard Management
    About
    SmartBida Manager is a billiard club management system built with Spring Boot (Java 22), React, MySQL 8, and Docker. The project provides essential features for club owners, managers, and staff: table booking & billing by time, product sales, QR/payment integration, and basic reporting.

---
+++ SBMS/src/test/java/org/example/sbms/SbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
