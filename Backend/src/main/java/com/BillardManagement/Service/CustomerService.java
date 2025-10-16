ec80add51958446ef3b1ea7a0cc2e27aca5a14bd
binh190305 <congbinh190305phan@gmail.com>
2025-10-16 09:31:37 +0700

    feature: hoàn thành xong crud cho club

---
+++ Backend/src/main/java/com/BillardManagement/Service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.BillardManagement.Service;
+import com.BillardManagement.Entity.Customer;
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerService {
+    List<Customer> getAllCustomers();
+
+    Optional<Customer> getCustomerById(Integer id);
+
+    Customer createCustomer(Customer customer);
+
+    Customer updateCustomer(Integer id, Customer customer);
+
+    void deleteCustomer(Integer id);
+}
