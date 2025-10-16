d8d5d47788e0ef7a1942821dc94545b73dca3d86
tundoumoi <vant7125@gmail.com>
2025-10-16 22:00:59 +0700

    Update CustomerRepo.java

--- Backend/src/main/java/com/BillardManagement/Repository/CustomerRepo.java
+++ Backend/src/main/java/com/BillardManagement/Repository/CustomerRepo.java
@@ -9,10 +9,7 @@ public interface CustomerRepo extends JpaRepository<Customer, Integer> {
     Optional<Customer> findByEmailAndPassword(String email, String password);
     // Kiểm tra trùng số điện thoại
     boolean existsByPhoneNumber(String phoneNumber);
-<<<<<<< Updated upstream
 
     // Kiểm tra trùng email
-=======
->>>>>>> Stashed changes
     boolean existsByEmail(String email);
 }
